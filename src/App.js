753ed904990c6a0d32e38061e11ef9442bc25dea
Ziad AlWazzan <51064118+ziadalwazzan@users.noreply.github.com>
2024-05-19 21:47:16 +0300

    update age component completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
 
-import { MenuList, MenuListItem, Separator, styleReset } from 'react95';
+import { styleReset } from 'react95';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
-
 
 /* Pick a theme of your choice */
 import original from 'react95/dist/themes/original';
@@ -12,6 +9,8 @@ import original from 'react95/dist/themes/original';
 /* Original Windows95 font (optional) */
 import ms_sans_serif from 'react95/dist/fonts/ms_sans_serif.woff2';
 import ms_sans_serif_bold from 'react95/dist/fonts/ms_sans_serif_bold.woff2';
+
+import UpdateAlert from './components/UpdateAlert';
 
 const GlobalStyles = createGlobalStyle`
   ${styleReset}
@@ -33,31 +32,13 @@ const GlobalStyles = createGlobalStyle`
 `;
 
 function App() {
-    const [position, setPosition] = useState({ top: '50%', left: '50%' });
-
-    const moveButton = () => {
-        const randX = Math.floor(Math.random() * (window.innerWidth - 100));
-        const randY = Math.floor(Math.random() * (window.innerHeight - 100));
-        setPosition({ top: `${randY}px`, left: `${randX}px` });
-    };
 
   return (
     <div className="App">
         <GlobalStyles />
         <ThemeProvider theme={original}>
-          <MenuList>
-            <MenuListItem>🎤 Sing</MenuListItem>
-            <MenuListItem>💃🏻 Dance</MenuListItem>
-            <Separator />
-            <MenuListItem disabled>😴 Sleep</MenuListItem>
-          </MenuList>
+          <UpdateAlert />
         </ThemeProvider>
-        <button
-              style={{ position: 'absolute', top: position.top, left: position.left }}
-              onMouseEnter={moveButton} // Change to onMouseEnter
-            >
-              Move Me
-            </button>
     </div>
   );
 }
---
+++ src/components/UpdateAlert.js
@@ -0,0 +1,71 @@
+import { Button, WindowHeader, Window, WindowContent } from 'react95';
+
+import { useState } from 'react';
+
+function UpdateAlert() {
+    const [position, setPosition] = useState({ top: '78%', left: '55%' });
+
+    const moveButton = () => {
+        const randX = Math.floor(Math.random() * (window.innerWidth - 100));
+        const randY = Math.floor(Math.random() * (window.innerHeight - 100));
+        setPosition({ top: `${randY}px`, left: `${randX}px` });
+    };
+    return (
+        <Window
+        style={{ 
+            backgroundColor: '#c6c6c6',
+            color: 'white',
+            padding: 20,
+            borderRadius: 10,
+            width: 400,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between'
+        }}
+        className="Window">
+                <WindowHeader
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    flexWrap: "wrap"
+                }}
+                >
+                <span>
+                    {`UPDATE REQUIRED`}
+                </span>
+                <Button
+                    style={{ marginTop: "1px" }}
+                    size={"sm"}
+                    square
+                    disabled='true'
+                >
+                    <span style={{ fontWeight: "bold", transform: "translateY(-1px)" }}>
+                    x
+                    </span>
+                </Button>
+                </WindowHeader>
+                <WindowContent>
+                    <text>It seems you are still 24. Please update your age to 25 to view this website. 🤣🫵🏼</text>
+                    <br/>
+                    <br/>
+                    <text>Would you like to update now?</text>
+                    <br/>
+                    <br/>
+                <Button
+                    style={{ position: 'absolute', top: position.top, left: position.left }}
+                    onMouseEnter={moveButton}
+                >
+                    No
+                </Button>
+                <Button
+                style={{ position: 'absolute', top: "78%", left: "35%" }}
+                >
+                    Yes
+                </Button>
+                </WindowContent>
+        </Window>
+    )
+}
+
+export default UpdateAlert;
